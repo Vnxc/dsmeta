3484bc538c92155a87c88ae61cc30e0862485857
Vinicius <vinicius.santosb@ufpe.br>
2022-09-10 13:08:37 -0300

    Frist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import NotificationButton from "./componentes/Notification button"
 
 function App() {
 return(
+  <>
   <h1>
     Olá mundo!!
   </h1>
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  <NotificationButton />
+  </>
 )
 }
 
